5cf1fdbd77021e448fc3a1d7fe24978002cea081
Ramazon098 <ramazon0619@gmail.com>
2022-12-16 20:12:31 +0500

    custom user model creating

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-
 from django.contrib.auth.models import AbstractUser
-
 from django.contrib.auth.password_validation import validate_password
 
 from phonenumber_field.modelfields import PhoneNumberField
