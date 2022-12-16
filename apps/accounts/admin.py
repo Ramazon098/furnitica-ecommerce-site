b3bcc6dca4edb1062c2b162b6d94e8b67521ad48
Ramazon098 <ramazon0619@gmail.com>
2022-12-16 20:15:41 +0500

    custom user adding admin site

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from apps.accounts.models import CustomUser
+
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    list_display = ['email',]
+    list_filter = ['email',]
+
+    fieldsets = UserAdmin.fieldsets
+
+    add_fieldsets = (
+        (None, {
+            'fields': ('email', 'first_name', 'first_name', 'password1', 'password2'),
+        }),
+    )
+
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
