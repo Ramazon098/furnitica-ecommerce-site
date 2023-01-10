255d19bff2db96e290ad92e6a948a6ab22bb3826
Ramazon098 <ramazon0619@gmail.com>
2023-01-10 19:17:20 +0500

    accounts folder update apps file

--- apps/accounts/apps.py
+++ apps/accounts/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
 
+# Create your apps here.
+
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.accounts'
