9c129946245e634f029f5e2f776b20ec87b497ac
Ramazon098 <ramazon0619@gmail.com>
2023-01-10 20:58:00 +0500

    custom user register serializer

---
+++ apps/accounts/serializers/register.py
@@ -0,0 +1,61 @@
+from django.contrib.auth.password_validation import validate_password
+
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
+from apps.accounts.models import CustomUser
+
+
+# Create your serializers here.
+
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        validators=[
+            UniqueValidator(
+                CustomUser.objects.all(),
+                "A user with that Email already exists.",
+            ),
+        ],
+    )
+
+    password1 = serializers.CharField(
+        style={'input_type': 'password'},
+        write_only=True,
+        validators=[validate_password],
+    )
+
+    password2 = serializers.CharField(
+        style={'input_type': 'password'},
+        write_only=True,
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = [
+            'id',
+            'email',
+            'first_name',
+            'last_name',
+            'password1',
+            'password2',
+        ]
+
+    def validate(self, attrs):
+        if attrs['password1'] != attrs['password2']:
+            raise serializers.ValidationError({
+                "password_error": "Password fields didn't match.",
+            })
+
+        return attrs
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create(
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+        )
+
+        user.set_password(validated_data['password1'])
+        user.save()
+
+        return user
