ee402e40e706238ec0e3708a5dce93913994595d
Ramazon098 <ramazon0619@gmail.com>
2023-01-12 16:35:20 +0500

    custom user change password url updated

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path('login/', views.LoginAPIView.as_view()),
     path('logout/', views.LogoutAPIView.as_view()),
 
-    path('change_password/<int:pk>/', views.ChangePasswordAPIView.as_view()),
+    path('change_password/', views.ChangePasswordAPIView.as_view()),
 ]
