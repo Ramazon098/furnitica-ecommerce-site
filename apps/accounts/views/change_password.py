93b3ed324c953834209a38760229e1d1a556f759
Ramazon098 <ramazon0619@gmail.com>
2023-01-17 19:24:53 +0500

    custom user change password api view updated

--- apps/accounts/views/change_password.py
+++ apps/accounts/views/change_password.py
@@ -5,8 +5,6 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 
-from knox.auth import AuthToken
-
 from apps.accounts.serializers import ChangePasswordSerializer
 
 
@@ -27,11 +25,8 @@ class ChangePasswordAPIView(APIView):
 
             update_session_auth_hash(request, user)
 
-            _, token = AuthToken.objects.create(user)
-
             return Response({
                 "password_success": "Your password has been successfully changed.",
-                'token': token,
             }, status=status.HTTP_200_OK)
 
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
