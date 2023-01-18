2b2ef00a0da200ee6a8eecbda60ad1ed94459e36
Ramazon098 <ramazon0619@gmail.com>
2023-01-18 20:09:11 +0500

    custom user create generic api view

--- apps/accounts/views/custom_user.py
+++ apps/accounts/views/custom_user.py
@@ -1,4 +1,5 @@
-from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -8,31 +9,33 @@ from apps.accounts.serializers import CustomUserSerializer
 
 # Create your views here.
 
-class CustomUserAPIView(APIView):
+class CustomUserAPIView(ListAPIView):
+    queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
 
-    def get(self, request):
-        custom_users = CustomUser.objects.all()
-        serializer = self.serializer_class(instance=custom_users, many=True)
+    # def get(self, request):
+    #     custom_users = CustomUser.objects.all()
+    #     serializer = self.serializer_class(instance=custom_users, many=True)
 
-        if request.user.is_authenticated and not request.user.is_superuser:
-            return Response({
-                "login_error": "You must log out to get a list of users.",
-            }, status=status.HTTP_401_UNAUTHORIZED)
+    #     if request.user.is_authenticated and not request.user.is_superuser:
+    #         return Response({
+    #             "login_error": "You must log out to get a list of users.",
+    #         }, status=status.HTTP_401_UNAUTHORIZED)
 
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
+    #     return Response(data=serializer.data, status=status.HTTP_200_OK)
 
 
-class CustomUserRetrieveAPIView(APIView):
+class CustomUserRetrieveAPIView(RetrieveAPIView):
+    queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
 
-    def get(self, request, pk):
-        custom_user = CustomUser.objects.get(pk=pk)
-        serializer = self.serializer_class(instance=custom_user)
+    # def get(self, request, pk):
+    #     custom_user = CustomUser.objects.get(pk=pk)
+    #     serializer = self.serializer_class(instance=custom_user)
 
-        if request.user.is_authenticated and not request.user.is_superuser:
-            return Response({
-                "login_error": "You must log out to retrieve user information.",
-            }, status=status.HTTP_401_UNAUTHORIZED)
+    #     if request.user.is_authenticated and not request.user.is_superuser:
+    #         return Response({
+    #             "login_error": "You must log out to retrieve user information.",
+    #         }, status=status.HTTP_401_UNAUTHORIZED)
 
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
+    #     return Response(data=serializer.data, status=status.HTTP_200_OK)
