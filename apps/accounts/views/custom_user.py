3e870b42a5c1e00d266d161a3039f48e0cb2fbda
Ramazon098 <ramazon0619@gmail.com>
2023-01-18 20:30:27 +0500

    cutom user api view updated

--- apps/accounts/views/custom_user.py
+++ apps/accounts/views/custom_user.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -8,27 +8,31 @@ from apps.accounts.serializers import CustomUserSerializer
 
 # Create your views here.
 
-class CustomUserAPIView(ListAPIView):
-    queryset = CustomUser.objects.all()
+class CustomUserAPIView(APIView):
     serializer_class = CustomUserSerializer
 
-    def list(self, request, *args, **kwargs):
+    def get(self, request):
+        custom_users = CustomUser.objects.all()
+        serializer = self.serializer_class(instance=custom_users, many=True)
+
         if request.user.is_authenticated and not request.user.is_superuser:
             return Response({
                 "login_error": "You must log out to get a list of users.",
             }, status=status.HTTP_401_UNAUTHORIZED)
 
-        return super().list(request, *args, **kwargs)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
 
-class CustomUserRetrieveAPIView(RetrieveAPIView):
-    queryset = CustomUser.objects.all()
+class CustomUserRetrieveAPIView(APIView):
     serializer_class = CustomUserSerializer
 
-    def retrieve(self, request, *args, **kwargs):
+    def get(self, request, pk):
+        custom_user = CustomUser.objects.get(pk=pk)
+        serializer = self.serializer_class(instance=custom_user)
+
         if request.user.is_authenticated and not request.user.is_superuser:
             return Response({
-                "login_error": "You must log out to get a list of users.",
+                "login_error": "You must log out to retrieve user information.",
             }, status=status.HTTP_401_UNAUTHORIZED)
 
-        return super().retrieve(request, *args, **kwargs)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
