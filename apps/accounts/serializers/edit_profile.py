410f7ed488935a00d22b9db1d7dd36b718ecbb5b
Ramazon098 <ramazon0619@gmail.com>
2023-01-20 19:35:49 +0500

    custom user edit profile serializer updated

--- apps/accounts/serializers/edit_profile.py
+++ apps/accounts/serializers/edit_profile.py
@@ -21,17 +21,18 @@ class EditProfileSerializer(ModelSerializer):
         ]
 
     def update(self, instance, validated_data):
-        instance.email = validated_data['email']
-        instance.first_name = validated_data['first_name']
-        instance.last_name = validated_data['last_name']
-        instance.phone_number = validated_data['phone_number']
-        instance.save()
+        for attr, value in validated_data.items():
+            if attr in 'address':
+                try:
+                    address = Address.objects.get(user=instance)
+                except Address.DoesNotExist:
+                    address = Address.objects.create(user=instance)
 
-        address = Address.objects.get(user=instance)
-        address.address = validated_data['address']['address']
-        address.city = validated_data['address']['city']
-        address.country = validated_data['address']['country']
-        address.zip_code = validated_data['address']['zip_code']
-        address.save()
+                for item, cost in value.items():
+                    setattr(address, item, cost)
+                address.save()
+            else:
+                setattr(instance, attr, value)
+        instance.save()
 
         return instance
