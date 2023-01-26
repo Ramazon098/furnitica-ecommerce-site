70815ee3b0740358ab9104e411e17a300ed654ed
Ramazon098 <ramazon0619@gmail.com>
2023-01-26 16:16:40 +0500

    calling the serializers in the serializer folder into one file

--- apps/accounts/serializers/__init__.py
+++ apps/accounts/serializers/__init__.py
@@ -3,7 +3,6 @@ from apps.accounts.serializers.custom_user import (
     CustomUserSerializer,
 )
 
-
 from apps.accounts.serializers.register import RegisterSerializer
 
 from apps.accounts.serializers.login import LoginSerializer
