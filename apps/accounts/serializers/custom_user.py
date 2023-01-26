90ce83ee18a0cbe70d0f76cdf1714d5bd0a12b32
Ramazon098 <ramazon0619@gmail.com>
2023-01-26 16:18:40 +0500

    serializer to display a list of information about users

--- apps/accounts/serializers/custom_user.py
+++ apps/accounts/serializers/custom_user.py
@@ -3,7 +3,6 @@ from rest_framework.serializers import ModelSerializer
 from apps.accounts.models import CustomUser, Address
 
 
-
 # Create your serializers here.
 
 class AddressSerializer(ModelSerializer):
