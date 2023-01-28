195e6699aa91e0f54ddb6a7e9826de5af9ea1959
Ramazon098 <ramazon0619@gmail.com>
2023-01-28 13:50:41 +0500

    user data views

--- apps/accounts/views/user.py
+++ apps/accounts/views/user.py
@@ -1,13 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-
 from rest_framework.response import Response
 from rest_framework import status
 
 from apps.accounts.models import CustomUser
 from apps.accounts.serializers import (
-    CustomUserSerializer,
-    EditProfileSerializer,
+    CustomUserSerializer, EditProfileSerializer,
 )
 
 
@@ -18,7 +16,10 @@ class CustomUserAPIView(APIView):
 
     def get(self, request):
         custom_users = CustomUser.objects.all()
-        serializer = self.serializer_class(instance=custom_users, many=True)
+        serializer = self.serializer_class(
+            instance=custom_users,
+            many=True,
+        )
 
         if request.user.is_authenticated and not request.user.is_superuser:
             return Response({
@@ -34,7 +35,9 @@ class CustomUserRetrieveAPIView(APIView):
     def get(self, request, pk):
         try:
             custom_user = CustomUser.objects.get(pk=pk)
-            serializer = self.serializer_class(instance=custom_user)
+            serializer = self.serializer_class(
+                instance=custom_user,
+            )
 
             if request.user.is_authenticated and not request.user.is_superuser:
                 return Response({
@@ -54,7 +57,9 @@ class EditProfileAPIView(APIView):
     serializer_class = EditProfileSerializer
 
     def get(self, request):
-        serializer = self.serializer_class(instance=request.user)
+        serializer = self.serializer_class(
+            instance=request.user,
+        )
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request):
