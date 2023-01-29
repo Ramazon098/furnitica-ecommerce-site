60ea5fef1f99ba4c6bb1b504210ba8fd3a0b2480
Ramazon098 <ramazon0619@gmail.com>
2023-01-29 14:59:56 +0500

    blog post model

--- apps/blogs/models.py
+++ apps/blogs/models.py
@@ -1,3 +1,99 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+CustomUser = get_user_model()
+
 
 # Create your models here.
+
+class Blog(models.Model):
+    author = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='blog_post',
+        verbose_name='Custom Users',
+    )
+
+    title = models.CharField(
+        max_length=128,
+        verbose_name='Blog Title',
+    )
+
+    body = models.TextField(
+        verbose_name='Blog Body',
+    )
+
+    image = models.ImageField(
+        upload_to='images/',
+        verbose_name='Blog Image',
+    )
+
+    created_at = models.DateTimeField(
+        auto_now=False,
+        auto_now_add=True,
+        verbose_name='Blog Post Create',
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        auto_now_add=False,
+        verbose_name='Blog Post Update',
+    )
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def comments(self):
+        instance = self
+        return Comment.objects.filter(blog=instance)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='user_comment',
+        verbose_name='Custom User',
+    )
+
+    blog = models.ForeignKey(
+        Blog,
+        on_delete=models.CASCADE,
+        related_name='blog_comment',
+        verbose_name='Blog Posts',
+    )
+
+    name = models.CharField(
+        max_length=128,
+        verbose_name='Comment Name',
+    )
+
+    email = models.EmailField(
+        max_length=128,
+        verbose_name='Comment Email',
+    )
+
+    website = models.URLField(
+        max_length=128,
+        verbose_name='Comment Website',
+    )
+
+    message = models.TextField(
+        verbose_name='Comment Message',
+    )
+
+    created_at = models.DateTimeField(
+        auto_now=False,
+        auto_now_add=True,
+        verbose_name='Blog Post Create',
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        auto_now_add=False,
+        verbose_name='Blog Post Update',
+    )
+
+    def __str__(self):
+        return self.name
