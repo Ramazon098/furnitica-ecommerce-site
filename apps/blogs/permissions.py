5a9915dccd9fa8c14c513e2a28ac941e8bee20cd
Ramazon098 <ramazon0619@gmail.com>
2023-01-29 21:00:09 +0500

    blog permissions

---
+++ apps/blogs/permissions.py
@@ -0,0 +1,17 @@
+from rest_framework.permissions import BasePermission
+
+
+# Create your permissions here.
+
+class NotOrIsAuthenticated(BasePermission):
+    message = 'You do not have permission to get the user list.'
+
+    def has_permission(self, request, view):
+        if request.method == 'GET':
+            return bool(
+                request.user and not not request.user.is_authenticated,
+            )
+        else:
+            return bool(
+                request.user and not request.user.is_authenticated,
+            )
