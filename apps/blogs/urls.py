a9f84e0496156dacda42e9902acb091e7234de03
Ramazon098 <ramazon0619@gmail.com>
2023-01-30 19:49:33 +0500

    blogs urls

--- apps/blogs/urls.py
+++ apps/blogs/urls.py
@@ -7,5 +7,6 @@ from apps.blogs import views
 urlpatterns = [
     path('', views.BlogPostAPIView.as_view()),
     path('<int:pk>/', views.BlogDetailAPIView.as_view()),
-    path('comments/',),
+    path('comments/', views.CommentAPIView.as_view()),
+    path('comment/<int:pk>/', views.CommentDetailAPIView.as_view()),
 ]
