f2a0eb5ba9636912ec5aa8d3ff03646ddd020b62
Ramazon098 <ramazon0619@gmail.com>
2023-01-30 20:39:11 +0500

    blog post serializers

--- apps/blogs/serializers.py
+++ apps/blogs/serializers.py
@@ -1,3 +1,5 @@
+import os
+
 from rest_framework import serializers
 
 from apps.blogs.models import Blog, Comment
@@ -36,3 +38,14 @@ class BlogSerializer(serializers.ModelSerializer):
             'created_at',
             'updated_at',
         ]
+
+    def validate_images(self, value):
+        extensions = os.path.splitext(value.name)[1]
+        validate_extensions = ['.jpg', '.png', '.jpeg', '.gif', '.raw']
+
+        if not extensions.lower() in validate_extensions:
+            raise serializers.ValidationError({
+                'extension_error': 'You can only upload images here.',
+            })
+
+        return value
